56d4a28a2c62925791f15f3d42495b0fb33978c6
Girish Sawant <girishsawant999.gs@gmail.com>
2022-07-18 13:05:52 +0530

    test: :white_check_mark: fixed failing testcase

--- src/Translate.test.tsx
+++ src/Translate.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Translate from './index';
+import Translate, { setupConfig } from './index';
 
 describe('Translate: Component Basis tests', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
+    setupConfig({
+      clientEmail: 'test@test.com',
+      projectId: 'test',
+      privateKey: 'KEY'
+    });
+
     ReactDOM.render(<Translate language="en-US">Hello</Translate>, div);
     expect(div).toBeTruthy();
     ReactDOM.unmountComponentAtNode(div);
